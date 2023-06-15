57d77023233560e322d034775d1b13166708f8fb
Hollye Edge <hollyekedge@gmail.com>
2023-06-15 13:29:18 -0600

    small chage to style standards

--- pages/stylestandards.js
+++ pages/stylestandards.js
@@ -5,12 +5,13 @@ function StyleStandards() {
       <h1 className="secondary_font">Use Noto Sans Display font for larger text</h1>
       <h3 className="primary_font">Use Noto Sans font for paragraphs and small text</h3>
       <div>
-        <h1 className="secondary_font">Website Colors</h1>
+        <h1 className="secondary_font">Primary Website Colors</h1>
         <label>#F2EBEC<div className="color1"></div></label>
         <label>#8A99A6<div className="color2"></div></label>
         <label>#D98D62<div className="color3"></div></label>
         <label>#8C4C35<div className="color4"></div></label>
         <label>#BF6A4B<div className="color5"></div></label>
+        <h1 className="secondary_font">Secondary Accent Colors</h1>
         <label>#6A7680<div className="color6"></div></label>
         <label>#D9D9D9<div className="color7"></div></label>
         <label>#939393<div className="color8"></div></label>
