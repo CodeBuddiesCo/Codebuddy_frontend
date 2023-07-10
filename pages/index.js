bd37e9586b7940349b8bd424ce86e55f54a72d09
Hollye Edge <hollyekedge@gmail.com>
2023-07-10 14:02:28 -0600

    Changes to home

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,22 @@ function HomePage() {
 
 return (
 <div>
-<div class="div-block-8"></div>
-  <section class="section wf-section">
-    <div class="div-block-3"></div>
-    <div class="div-block-4">
-      <a href="#" class="nav-buttons w-button">What is codebuddy?</a>
-      <a href="#" class="nav-buttons w-button">Become a buddy</a>
-      <a href="#" class="nav-buttons w-button">Calendar of Events</a>
-      <a href="#" class="nav-buttons right w-button">Signup / Login</a>
+  <div class="main-photo"></div>
+  <section class="header-section wf-section">
+    <div class="logo-div"></div>
+    <div class="nav-btn-div">
+      <a href="#" class="header-nav-buttons w-button">What is codebuddy?</a>
+      <a href="#" class="header-nav-buttons w-button">Become a buddy</a>
+      <a href="#" class="header-nav-buttons w-button">Calendar of Events</a>
+      <a href="#" class="header-nav-buttons button-right w-button">Signup / Login</a>
     </div>
   </section>
-  <section class="section-2 wf-section">
-    <div class="div-block-5"></div>
-    <div class="div-block-6">
-      <div class="text-block">Experience the power of <span class="text-span">collaboration</span> on                   , connecting you with skilled  code enthusiasts through live video to supercharge your code</div>
-      <div class="div-block-7"></div>
+  <section class="home-section wf-section">
+    <div class="home-top-div">
+      <div class="home-spacer-div"></div>
+      <div class="tagline-div">
+        <div class="tagline-text-block">Experience the power of <span class="collab-text-span">collaboration</span> on <span class="logo-text-span"> sdbhjjkk</span>, connecting you with skilled code enthusiasts through live video to supercharge your code</div>
+      </div>
     </div>
   </section>
 </div>
