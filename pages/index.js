90d0cc8f3706b8c0d2a18692e0cd39619ff47f4d
Hollye Edge <hollyekedge@gmail.com>
2023-07-10 12:17:23 -0600

    homepage

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,26 @@ function HomePage() {
   const eventId = new Date();
 
 return (
-  <div>
-    <h1>Welcome to Codebuddy</h1>
-    <ul>
-      <li><Link href="/event/calendar">Calendar of Events</Link></li>
-      <li><Link href={`/event/details/${eventId}`}>Event Details</Link></li>
-      <li><Link href="/user/login">Login</Link></li>
-      <li><Link href="/user/register">Register</Link></li>
-      <li><Link href="/user/profile">User Profile</Link></li>
-    </ul>
-  </div>
+<div>
+<div class="div-block-8"></div>
+  <section class="section wf-section">
+    <div class="div-block-3"></div>
+    <div class="div-block-4">
+      <a href="#" class="nav-buttons w-button">What is codebuddy?</a>
+      <a href="#" class="nav-buttons w-button">Become a buddy</a>
+      <a href="#" class="nav-buttons w-button">Calendar of Events</a>
+      <a href="#" class="nav-buttons right w-button">Signup / Login</a>
+    </div>
+  </section>
+  <section class="section-2 wf-section">
+    <div class="div-block-5"></div>
+    <div class="div-block-6">
+      <div class="text-block">Experience the power of <span class="text-span">collaboration</span> on                   , connecting you with skilled  code enthusiasts through live video to supercharge your code</div>
+      <div class="div-block-7"></div>
+    </div>
+  </section>
+</div>
+
 )
 }
 
