7ac86ee5062c7b2756ed918bf9a4b4b9aa9da1a4
cvmug <catherine.mugnai@gmail.com>
2023-07-13 11:42:52 -0400

    login and register working

--- pages/user/register.js
+++ pages/user/register.js
@@ -1,11 +1,96 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://codebuddiesserver.onrender.com/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          username,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.token) {
+          setMessage('You have successfully registered');
+          setName('');
+          setEmail('');
+          setUsername('');
+          setPassword('');
+          router.push('/user/login');
+        } else {
+          throw new Error();
+        }
+      } else {
+        if(response.status === 409){
+          setMessage('Username or Email already exists');
+        } else {
+          throw new Error();
+        }
+      }
+    } catch (error) {
+      setMessage('Registration failed');
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <div>
-      <h1>this is where you will register for an account</h1>
-  
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {message && <p>{message}</p>}
+        <button type="submit">Register</button>
+      </form>
     </div>
-  )
-  }
-  
-  export default Register;
+  );
+}
+
+export default Register;
