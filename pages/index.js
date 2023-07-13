43723252dc9c0e2dc7277adab6349f0b768bf061
Hollye Edge <hollyekedge@gmail.com>
2023-07-13 10:05:23 -0600

    fixed images

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,21 @@ function HomePage() {
 
 return (
 <div>
-  <div class="main-photo"></div>
-  <section class="header-section wf-section">
-    <div class="logo-div"></div>
-    <div class="nav-btn-div">
-      <a href="#" class="header-nav-buttons w-button">What is codebuddy?</a>
-      <a href="#" class="header-nav-buttons w-button">Become a buddy</a>
-      <a href="#" class="header-nav-buttons w-button">Calendar of Events</a>
-      <a href="#" class="header-nav-buttons button-right w-button">Signup / Login</a>
+  <div><img className="main-photo" src="alvan-nee-73flblFUksY-unsplash-1-min.jpg" alt='image of dog friends happy together'/></div>
+  <section className="header-section wf-section">
+  <img className='logo-div' src="/CB_logo.png" alt='code buddy logo'/>
+    <div className="nav-btn-div">
+      <a href="#" className="header-nav-buttons w-button">What is codebuddy?</a>
+      <a href="#" className="header-nav-buttons w-button">Become a buddy</a>
+      <a href="#" className="header-nav-buttons w-button">Calendar of Events</a>
+      <a href="#" className="header-nav-buttons button-right w-button">Signup / Login</a>
     </div>
   </section>
-  <section class="home-section wf-section">
-    <div class="home-top-div">
-      <div class="home-spacer-div"></div>
-      <div class="tagline-div">
-        <div class="tagline-text-block">Experience the power of <span class="collab-text-span">collaboration</span> on <span class="logo-text-span"> sdbhjjkk</span>, connecting you with skilled code enthusiasts through live video to supercharge your code</div>
+  <section className="home-section wf-section">
+    <div className="home-top-div">
+      <div className="home-spacer-div"></div>
+      <div className="tagline-div">
+        <div className="tagline-text-block">Experience the power of <span className="collab-text-span">collaboration</span> on <span> <img className='logo-text-span' src='grey_logo.png' alt='code buddy logo'/></span>, connecting you with skilled code enthusiasts through live video to supercharge your code</div>
       </div>
     </div>
   </section>
