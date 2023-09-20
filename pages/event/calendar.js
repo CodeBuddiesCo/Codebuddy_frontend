01d1b5ce3e803bb3c8b74bc39857c8d3770df0fa
Hollye Edge <hollyekedge@gmail.com>
2023-09-19 19:36:26 -0600

    I don't remember what these changes were

--- pages/event/calendar.js
+++ pages/event/calendar.js
@@ -55,7 +55,7 @@ function CalendarOfEvents({allEvents, setAllEvents, loading, setLoading, isBuddy
       console.error   
     }
   }
-   console.log(currentPage)
+
   useEffect(() => {
     setIsBuddy(JSON.parse(window.localStorage.getItem("isBuddy")));
     getAllEvents();
