0a6a29d75096d261c19507d81a3375221639bef9
cvmug <catherine.mugnai@gmail.com>
2023-12-20 22:54:04 -0500

    updated-login

--- pages/user/register.js
+++ pages/user/register.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Alert } from 'react-bootstrap';
 import Link from 'next/link';
-import styles from '../../styles/authForms.module.css';
+import styles from '../../styles/authForms.module.css'; // Import your custom styles here
 import { signIn } from 'next-auth/react';
+import Header from '../../components/Header';
 
 function Register() {
-  
   const securityQuestions = [
     "What was your first pet's name?",
     "What was the make of your first car?",
@@ -33,7 +31,6 @@ function Register() {
   const [securityAnswer2, setSecurityAnswer2] = useState('');
   const [securityAnswer3, setSecurityAnswer3] = useState('');
 
-
   const handlePasswordBlur = () => {
     if (password.length < 6) {
       setPasswordError('Password must be at least 6 characters');
@@ -49,7 +46,6 @@ function Register() {
         // Handle successful registration
         // router.push('/user/profile');
         console.log(result);
-
       } else {
         // Handle registration error
         console.error(result.error);
@@ -64,7 +60,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://codebuddiesserver.onrender.com/api/users/register', { // Correct the URL if needed
+      const response = await fetch('https://codebuddiesserver.onrender.com/api/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -109,151 +105,142 @@ function Register() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={`row m-5 no-gutters shadow-lg my-card`}>
-        <div className="col-md-6 d-none d-md-block left">
-          <img src="/registerdog2.png"
-            className="img-fluid"
-            style={{ minHeight: "100%", width: "100%", height: "100%", objectFit: "cover" }} />
-        </div>
-        <div className={`col-md-6 bg-white p-5 right ${styles['form-style']}`}>
-          <h3 className="pb-3">Register</h3>
+    <div className={styles.registerPage}>
+      <Header />
+      <div className={styles.registerMainContentContainer}>
+        <img
+          className={styles.registerFormImageContainer}
+          src="/registerdog2.png"
+          alt="Register Image"
+        />
+        <div className={styles.registerFormContainer}>
+          <div className={styles.registerFormHeaderContainer}>
+            <h1 className={styles.registerFormHeader}>Register</h1>
+          </div>
+          {message && <div className={styles.registerAlert}>{message}</div>}
           <form onSubmit={handleSubmit}>
-            <div className="form-group pb-3">
-              <input
-                type="text"
-                placeholder="Name"
-                className="form-control"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group pb-3">
-              <input
-                type="text"
-                placeholder="Email"
-                className="form-control"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group pb-3">
-              <input
-                type="text"
-                placeholder="Username"
-                className="form-control"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group pb-3">
-              <input
-                type="password"
-                placeholder="Password"
-                className="form-control"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onBlur={handlePasswordBlur}
-                required
-              />
-            </div>
-            {passwordError && <div className="text-danger">{passwordError}</div>}
-
-            {/* Security Question 1 */}
-            <div className="form-group pb-3">
-              <select
-                className="form-control"
-                value={securityQuestion1}
-                onChange={(e) => setSecurityQuestion1(e.target.value)}
-                required
-              >
-                {securityQuestions.map((question, index) => (
-                  <option key={index} value={question}>{question}</option>
-                ))}
-              </select>
-              <input
-                type="text"
-                placeholder="Security Answer 1"
-                className="form-control"
-                value={securityAnswer1}
-                onChange={(e) => setSecurityAnswer1(e.target.value)}
-                required
-              />
-            </div>
-
-            {/* Security Question 2 */}
-            <div className="form-group pb-3">
-              <select
-                className="form-control"
-                value={securityQuestion2}
-                onChange={(e) => setSecurityQuestion2(e.target.value)}
-                required
-              >
-                {securityQuestions.map((question, index) => (
-                  <option key={index} value={question}>{question}</option>
-                ))}
-              </select>
-              <input
-                type="text"
-                placeholder="Security Answer 2"
-                className="form-control"
-                value={securityAnswer2}
-                onChange={(e) => setSecurityAnswer2(e.target.value)}
-                required
-              />
-            </div>
-
-            {/* Security Question 3 */}
-            <div className="form-group pb-3">
-              <select
-                className="form-control"
-                value={securityQuestion3}
-                onChange={(e) => setSecurityQuestion3(e.target.value)}
-                required
-              >
-                {securityQuestions.map((question, index) => (
-                  <option key={index} value={question}>{question}</option>
-                ))}
-              </select>
-              <input
-                type="text"
-                placeholder="Security Answer 3"
-                className="form-control"
-                value={securityAnswer3}
-                onChange={(e) => setSecurityAnswer3(e.target.value)}
-                required
-              />
-            </div>
-
-            <div className="pb-2">
-              <button
-                type="submit"
-                className={`btn btn-dark w-100 font-weight-bold mt-2 ${styles['form-button']}`}
-              >
-                Submit
-              </button>
-              <div className={styles.sideline}>OR</div>
-              <div>
-                <button
-                  onClick={() => handleOAuthRegister('google')}
-                  className={`btn btn-dark w-100 font-weight-bold mt-2 ${styles['email-form-button']}`}
-                >
-                  Register With Google
-                </button>
+            <div className={styles.registerFormInputContainer}>
+              <div className={styles.registerSelectBorder}>
+                <input
+                  className={styles.registerSelect}
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
               </div>
+
+              <div className={styles.registerSelectBorder}>
+                <input
+                  className={styles.registerSelect}
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className={styles.registerSelectBorder}>
+                <input
+                  className={styles.registerSelect}
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className={styles.registerSelectBorder}>
+                <input
+                  className={styles.registerSelect}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  onBlur={handlePasswordBlur}
+                  required
+                />
+              </div>
+              {passwordError && <div className={styles.registerPasswordError}>{passwordError}</div>}
+
+              {/* Security Question 1 */}
+              <div className={styles.registerFormInputContainer}>
+                <select
+                  className={styles.registerSelect}
+                  value={securityQuestion1}
+                  onChange={(e) => setSecurityQuestion1(e.target.value)}
+                  required
+                >
+                  {securityQuestions.map((question, index) => (
+                    <option key={index} value={question}>{question}</option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  placeholder="Security Answer 1"
+                  className={styles.registerSelect}
+                  value={securityAnswer1}
+                  onChange={(e) => setSecurityAnswer1(e.target.value)}
+                  required
+                />
+              </div>
+
+              {/* Security Question 2 */}
+              <div className={styles.registerFormInputContainer}>
+                <select
+                  className={styles.registerSelect}
+                  value={securityQuestion2}
+                  onChange={(e) => setSecurityQuestion2(e.target.value)}
+                  required
+                >
+                  {securityQuestions.map((question, index) => (
+                    <option key={index} value={question}>{question}</option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  placeholder="Security Answer 2"
+                  className={styles.registerSelect}
+                  value={securityAnswer2}
+                  onChange={(e) => setSecurityAnswer2(e.target.value)}
+                  required
+                />
+              </div>
+
+              {/* Security Question 3 */}
+              <div className={styles.registerFormInputContainer}>
+                <select
+                  className={styles.registerSelect}
+                  value={securityQuestion3}
+                  onChange={(e) => setSecurityQuestion3(e.target.value)}
+                  required
+                >
+                  {securityQuestions.map((question, index) => (
+                    <option key={index} value={question}>{question}</option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  placeholder="Security Answer 3"
+                  className={styles.registerSelect}
+                  value={securityAnswer3}
+                  onChange={(e) => setSecurityAnswer3(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className={styles.registerSubmitButtonContainer}>
+                <button className={styles.registerSubmitButton} type="submit">Register</button>
+              </div>
+
+              <p className={styles.alreadyHaveAccountText}>
+                Already have an account? <Link href="/user/login" className={styles.registerSignUpLink}>Sign In</Link>
+              </p>
             </div>
           </form>
-          <div className="pt-4 text-center">
-            Already have an account? <Link href="/user/login">Sign In</Link>
-          </div>
-          {message && (
-            <Alert variant="danger" className="mt-3">
-              {message}
-            </Alert>
-          )}
         </div>
       </div>
     </div>
